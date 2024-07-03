eda76522741fbe2b3f785f69e26a13ee63453cef
it <kyle9742@naver.com>
2024-07-03 14:22:27 +0900

    네브바 2차 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,57 @@
 /* eslint-disable react/no-unescaped-entities */
-import "./Navbar.css";
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css'; // CSS 파일 임포트
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 50;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav id="navbar-example2" className="navbar navbar-dark bg-dark fixed-top">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <ul className="nav nav-pills">
-        <li className="nav-item">
-          <a className="nav-link" href="#home">
-            Home
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#about">
-            About
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#project">
-            Project
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#contact">
-            Contact
-          </a>
-        </li>
-      </ul>
+    <nav className={`navbar navbar-expand-lg navbar-dark fixed-top ${scrolled ? 'bg-white' : 'bg-transparent'}`}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          SeoYoon's Portfolio
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#project">
+                Project
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
