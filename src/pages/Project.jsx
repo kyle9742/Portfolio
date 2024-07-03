b2947b0d57c53656e383f2f1fae24900e1421647
it <kyle9742@naver.com>
2024-07-03 16:37:19 +0900

    project title, img, 설명 추가

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import './Project.css'
+import React from "react";
+import "./Project.css";
 
 const Project = () => {
   return (
-    <div className='project' id='project'>
-      프로젝트입니다.
+    <div className="project text-center" id="project">
+      <div className="projectTitle">매직메모리 WEB게임</div>
+      <div className="projectTotal d-flex justify-content-center align-center">
+        <div className="projectImg">
+          <img src="src\assets\magic-memory.png" alt="프로젝트 이미지" />
+        </div>
+        <div className="project">asdadasdasdsad</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
