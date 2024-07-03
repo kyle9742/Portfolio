3df4a0f16df1ed637a74ff7d7cf75b6de8ebcde3
it <kyle9742@naver.com>
2024-07-03 13:38:51 +0900

    네브바 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,34 @@
+/* eslint-disable react/no-unescaped-entities */
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        SeoYoon's Portfolio
-      </div>
-      <div className="nav">
-        <a href="#home">Home</a>
-        <a href="#about">about</a>
-        <a href="#project">project</a>
-        <a href="#contact">contact</a>
-      </div>
+    <nav id="navbar-example2" className="navbar navbar-dark bg-dark fixed-top">
+      <a className="navbar-brand" href="#">
+        Navbar
+      </a>
+      <ul className="nav nav-pills">
+        <li className="nav-item">
+          <a className="nav-link" href="#home">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#about">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#project">
+            Project
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#contact">
+            Contact
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
